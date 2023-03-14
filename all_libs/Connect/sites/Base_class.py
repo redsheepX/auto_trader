15dc1a98d3d5560ee1fd18d1a24d38f9ec7cd9d3
zxdr0417 <zxdr0417@gmail.com>
2023-03-14 18:55:36 +0800

    增加部分BingX內容

--- all_libs/Connect/sites/Base_class.py
+++ all_libs/Connect/sites/Base_class.py
@@ -5,8 +5,24 @@ class Connection(metaclass=abc.ABCMeta):
     
     def __init__(self) -> None:
         pass
+
     @abc.abstractmethod
-    def getBalance(self) -> dict:
-        """獲得帳戶數據
-        """
+    def account_balance(self):
+        """獲得帳戶餘額"""
+        return NotImplemented
+    
+    @abc.abstractmethod
+    def get_historyKline(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def trade_buy(self):
+        return NotImplemented
+    
+    @abc.abstractmethod
+    def trade_sell(self):
+        return NotImplemented
+    
+    @abc.abstractmethod
+    def account_position(self):
         return NotImplemented
